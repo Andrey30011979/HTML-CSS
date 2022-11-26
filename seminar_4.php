fb9c5f62a6929817007f6c826b96eb873dfca31d
Andrey30011979 <109824571+Andrey30011979@users.noreply.github.com>
2022-11-26 23:28:52 +0300

    seminar 5

---
+++ seminar_4.php
@@ -0,0 +1,42 @@
+<?php
+
+$my_name = 'Андрей'; // строка
+
+$my_city = 'Москва, Россия'; // строка
+
+$my_profession = 'Студент'; // строка
+
+$my_email = 'email@email.com'; // строка
+
+$my_phone = 89037778866; // десятичное число
+
+$my_age = 2022-1979;
+
+$my_days = (2022-1979)*365;
+
+$skills = [
+
+'skill_name' => ['Adobe Photoshop', 'Фотография', 'JavaScript', 'C#'],
+
+'level' => [11, 12, 10, 5]
+
+]
+
+?>
+<?php
+
+$my_work = 'Опыт работы';
+$my_current = 'jan 2022-';
+$my_work2 = 'Mar 2012 - Dec 2014';
+$my_work1 = 'Jun 2010 - Mar 2012';
+$skills1 = [
+
+'skill_language' => ['Английский', 'Русский',  'Славянский'],
+
+'level1' => [10, 9, 99]
+
+]
+
+
+
+?>
